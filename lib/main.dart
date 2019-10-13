b036891218913cbbecfdfae29fb90389313e7d49
caveinn <caveinncicad@gmail.com>
2019-10-13 22:56:46 +0300

    first iteration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,94 +19,72 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+class MyHomePage extends StatelessWidget {
+  final TextStyle _myStyle = TextStyle(
+                fontFamily: 'Zhi Mang Xing',
+                fontSize:30,
+                fontStyle: FontStyle.italic,
+              );
+  final double _dividerSize = 5;
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Introduction To Computers"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.yellow[300],
+      body: Padding( 
+        padding:EdgeInsets.only(top: 20) ,
+        child:Center(child: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              "Introduction",
+              style: _myStyle ,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+            subtitle: Text("Why is it good to learn how computers work"),
+            onTap: (){},
+          ),
+          Divider(thickness: _dividerSize,),
+          ListTile(
+            title: Text("Lesson 1", style: _myStyle),
+            subtitle: Text("What Do the parts DO"),
+            onTap: () {},
+          ),
+          Divider(thickness: _dividerSize,),
+          ListTile(
+            title: Text("Lesson 2", style: _myStyle,),
+            subtitle: Text("The DOS operating system"),
+            onTap: (){},
+          ),
+            Divider(thickness: _dividerSize,),
+          ListTile(
+            title: Text("Lesson 3", style: _myStyle,),
+            subtitle: Text("Files, the Overview"),
+            onTap: (){},
+          ),
+          Divider(thickness: _dividerSize,),
+          ListTile(
+            title: Text("Lesson 4", style: _myStyle,),
+            subtitle: Text("They are Like LP Records"),
+            onTap: (){},
+          ),
+          Divider(thickness: _dividerSize,),
+          ListTile(
+            title: Text("Lesson 5", style: _myStyle,),
+            subtitle: Text("Organizing Files is Like Organizing Refrigerators"),
+            onTap: (){},
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    ),
+      )
     );
   }
 }
