2ee9c059ce9c1d8455340f90025f3d637d10a193
caveinn <caveinncicad@gmail.com>
2019-10-15 22:56:09 +0300

    add nav

--- lib/pages.dart
+++ lib/pages.dart
@@ -8,13 +8,47 @@ class introdution extends StatelessWidget {
       appBar: AppBar(
         title: Text("Introduction"),
       ),
-      body: SingleChildScrollView(
-        child: Card(
+      body: ListView(children: <Widget>[
+        Card(
           child: Padding(
             padding: EdgeInsets.all(12),
             child: getIntroduction_data(),
           ),
         ),
+      ]),
+      bottomNavigationBar: ButtonBar(
+        alignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          InkWell(
+            customBorder: CircleBorder(),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                "Previous",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            onTap: () {},
+          ),
+          InkWell(
+            customBorder: CircleBorder(),
+            
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                "Next",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
